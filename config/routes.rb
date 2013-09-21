2b211362097384bdb200f605bb7730f00754d800
horologium <horologium.04@gmail.com>
2013-09-21 19:34:22 +0200

    add route to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Microboard::Application.routes.draw do
   resources :posts
 
-	root	'application#index'
+	root	posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
